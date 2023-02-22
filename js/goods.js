b494d9dc2bcd7fbd1ceb620e87043be908d835ed
松村彩音 <nennen@matsumuraayanenonotopasokon.local>
2023-02-22 17:51:01 +0900

    add info

--- js/goods.js
+++ js/goods.js
@@ -58,3 +58,267 @@ $(function(){
 		}
 	});
 });
+
+$(function(){
+	var open = $('.item-springin'),
+		close = $('.goods-info-close'),
+		container = $('.info-springin');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-toio'),
+		close = $('.goods-info-close'),
+		container = $('.info-toio');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-alilo'),
+		close = $('.goods-info-close'),
+		container = $('.info-alilo');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-romi'),
+		close = $('.goods-info-close'),
+		container = $('.info-romi');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-swich'),
+		close = $('.goods-info-close'),
+		container = $('.info-swich');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-car'),
+		close = $('.goods-info-close'),
+		container = $('.info-car');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-logical'),
+		close = $('.goods-info-close'),
+		container = $('.info-logical');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-think'),
+		close = $('.goods-info-close'),
+		container = $('.info-think');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-Q'),
+		close = $('.goods-info-close'),
+		container = $('.info-Q');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-why'),
+		close = $('.goods-info-close'),
+		container = $('.info-why');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
+
+$(function(){
+	var open = $('.item-tekisiko'),
+		close = $('.goods-info-close'),
+		container = $('.info-tekisiko');
+
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+		$('body').css('overflow','hidden');
+	});
+
+	close.on('click',function(){	
+		container.removeClass('active');
+		$('body').css('overflow','auto');
+	});
+
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.goods-info').length) {
+			container.removeClass('active');
+			$('body').css('overflow','auto');
+		}
+	});
+});
